753340795162ab998be9197a81ef75b9c747e98f
MyrtheVanMoorst <65168254+MyrtheVanMoorst@users.noreply.github.com>
2021-11-09 14:17:58 +0100

    Adding big box processing

--- drone-area-mapping/src/routes/capture-images.jsx
+++ drone-area-mapping/src/routes/capture-images.jsx
@@ -1,22 +1,22 @@
-import { motion } from 'framer-motion';
-import { pageTransition } from '../helper/animations';
-import { BigBox } from '../components/big-box';
-import { SmallButton } from '../components/small-button';
+import { motion } from "framer-motion";
+import { pageTransition } from "../helper/animations";
+import { BigBox } from "../components/big-box";
+import { SmallButton } from "../components/small-button";
 
 const CaptureImages = () => {
   return (
     <motion.div
-      initial='initial'
-      animate='animate'
-      exit='exit'
+      initial="initial"
+      animate="animate"
+      exit="exit"
       variants={pageTransition}
-      className='w-full h-5/6 flex items-center justify-center'
+      className="w-full h-5/6 flex items-center justify-center"
     >
-      <div className='w-5/6 h-full flex flex-row justify-between items-center'>
+      <div className="w-5/6 h-full flex flex-row justify-between items-center">
         {/* Left */}
-        <BigBox title='Images'>
+        <BigBox title="Images">
           <div>
-            <SmallButton name='Start' color='startBtn' />
+            <SmallButton name="Start" color="startBtn" />
           </div>
         </BigBox>
         {/* Right */}
