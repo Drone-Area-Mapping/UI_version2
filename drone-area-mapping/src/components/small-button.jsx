37200318f039419da0f05c7bbcbc312da7b6f460
Xytrical <ixandermc@gmail.com>
2021-11-09 14:59:27 +0100

    ddd

--- drone-area-mapping/src/components/small-button.jsx
+++ drone-area-mapping/src/components/small-button.jsx
@@ -4,7 +4,7 @@ export const SmallButton = ({ name, color }) => {
   return (
     <motion.button
       whileTap={{ scale: 0.9 }}
-      className={`w-full h-full max-h-20 focus:outline-none rounded-full border-2 border-${color} hover:bg-${color} shadow-lg text-xl text-heading font-light`}
+      className={`w-1/2 h-full max-h-20 focus:outline-none rounded-full border-2 border-${color} hover:bg-${color} shadow-lg text-xl text-heading font-light`}
     >
       {name}
     </motion.button>
