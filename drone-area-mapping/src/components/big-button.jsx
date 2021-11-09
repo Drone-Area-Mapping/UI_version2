1f729e8e6f5588fdd8710a2749651dafa619901c
Xytrical <ixandermc@gmail.com>
2021-11-09 12:18:58 +0100

    changed some responsiveness

--- drone-area-mapping/src/components/big-button.jsx
+++ drone-area-mapping/src/components/big-button.jsx
@@ -1,10 +1,10 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 export const BigButton = ({ text }) => {
   return (
     <motion.button
       whileTap={{ scale: 0.9 }}
-      className="w-80 h-20 rounded-full border-2 border-blue-300 hover:bg-blue-300 shadow-lg text-2xl text-heading font-light"
+      className='w-80 h-20 rounded-full border-2 border-blue-300 hover:bg-blue-300 shadow-lg text-2xl text-heading font-light'
     >
       {text}
     </motion.button>
--- drone-area-mapping/src/components/navbar.jsx
+++ drone-area-mapping/src/components/navbar.jsx
@@ -1,22 +1,22 @@
-import AvansLogo from "../assets/images/avans-logo.png";
-import RanLogo from "../assets/images/ran-logo.png";
-import Icon from "@material-tailwind/react/Icon";
-import { Link, useLocation } from "react-router-dom";
-import { motion } from "framer-motion";
+import AvansLogo from '../assets/images/avans-logo.png';
+import RanLogo from '../assets/images/ran-logo.png';
+import Icon from '@material-tailwind/react/Icon';
+import { Link, useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const GetIndex = () => {
   const location = useLocation();
   const locationStr = location.pathname.slice(1);
 
-  if (locationStr === "image-processing") return 2;
-  else if (locationStr === "capture-images") return 3;
+  if (locationStr === 'image-processing') return 2;
+  else if (locationStr === 'capture-images') return 3;
   else return 1;
 };
 
 const NavIcon = ({ id, name }) => {
   return (
     <motion.div whileHover={GetIndex() !== id ? { y: -10 } : { y: 0 }}>
-      <Icon name={name} size="5xl" color="white" />
+      <Icon name={name} size='5xl' color='white' />
     </motion.div>
   );
 };
@@ -25,7 +25,7 @@ const NavLink = ({ id, route, name }) => {
   return (
     <Link
       className={`relative h-full w-1/5 justify-center flex items-center ${
-        GetIndex() !== id && "opacity-50 hover:opacity-100"
+        GetIndex() !== id && 'opacity-50 hover:opacity-100'
       }`}
       to={route}
     >
@@ -34,7 +34,7 @@ const NavLink = ({ id, route, name }) => {
         <motion.div
           initial={{ scaleX: 0 }}
           animate={{ scaleX: 1 }}
-          className="absolute bottom-0 h-1 bg-blue-300 w-full"
+          className='absolute bottom-0 h-1 bg-blue-300 w-full'
         ></motion.div>
       )}
     </Link>
@@ -43,14 +43,14 @@ const NavLink = ({ id, route, name }) => {
 
 export const NavBar = () => {
   return (
-    <div className="w-full h-32 bg-background shadow-xl flex flex-row items-center justify-between pr-4 pl-4">
-      <img className="h-1/2 object-contain" src={AvansLogo} alt="" />
-      <div className="flex flex-row space-x-20 h-full items-center justify-center w-1/3">
-        <NavLink id={1} route="/" name="home" />
-        <NavLink id={2} route="/image-processing" name="broken_image" />
-        <NavLink id={3} route="/capture-images" name="linked_camera" />
+    <div className='w-full h-1/6 bg-background shadow-xl flex flex-row items-center justify-between pr-4 pl-4'>
+      <img className='h-1/2 object-contain' src={AvansLogo} alt='' />
+      <div className='flex flex-row space-x-20 h-full items-center justify-center w-1/3'>
+        <NavLink id={1} route='/' name='home' />
+        <NavLink id={2} route='/image-processing' name='broken_image' />
+        <NavLink id={3} route='/capture-images' name='linked_camera' />
       </div>
-      <img className="h-1/2 object-contain" src={RanLogo} alt="" />
+      <img className='h-1/2 object-contain' src={RanLogo} alt='' />
     </div>
   );
 };
