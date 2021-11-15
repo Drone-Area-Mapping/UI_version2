d41b22a793cd4c68f27a8ff7d27bb82e6dc5f41d
MyrtheVanMoorst <65168254+MyrtheVanMoorst@users.noreply.github.com>
2021-11-15 15:11:35 +0100

    file path

--- drone-area-mapping/src/components/Long-Small-box.jsx
+++ drone-area-mapping/src/components/Long-Small-box.jsx
@@ -1,14 +1,35 @@
 import Icon from "@material-tailwind/react/Icon";
+import { useRef, useState } from "react";
 
-export const LongSmallBox = ({ text, title, children }) => {
+export const LongSmallBox = ({ text, title }) => {
+  const input = useRef();
+  const [isValidFilepath, setValidFilepath] = useState(false);
+  const [filepath, setFilepath] = useState("");
+
   return (
-    <div className="h-full w-full bg-box shadow-xl rounded-2xl p-6">
+    <div className="h-full w-full bg-box shadow-xl rounded-2xl p-6 flex flex-col justify-evenly space-y-2">
       {/* LongBigBoxTitle */}
-      <div className="flex flex-row space-x-4 items-center">
+      <div className="flex flex-row space-x-4 items-end">
         <Icon name={text} size="4xl" color="white" />
         <p className="font-title text-2xl lg:text-3xl xl:text-4xl font-normal">
           {title}
         </p>
+      </div>
+      <div className="w-full flex flex-row space-x-4 h-1/2 items-center">
+        <input
+          ref={input}
+          value={filepath}
+          placeholder="filepath to images"
+          type="text"
+          className="w-full h-full rounded-lg bg-transparent border-2 border-gray-400 focus:border-blue-300 outline-none font-text text-lg lg:text-xl xl:text-2xl p-2"
+          onFocus={() => input.current.select()}
+          onChange={(e) => setFilepath(e.target.value)}
+        />
+        {isValidFilepath ? (
+          <Icon name="check_circle" size="4xl" color="lightGreen" />
+        ) : (
+          <Icon name="cancel" size="4xl" color="red" />
+        )}
       </div>
     </div>
   );
--- drone-area-mapping/src/components/Long-big-box.jsx
+++ drone-area-mapping/src/components/Long-big-box.jsx
@@ -1,14 +1,20 @@
 import Icon from "@material-tailwind/react/Icon";
+import Checkbox from "@material-tailwind/react/Checkbox";
 
-export const LongBigBox = ({ text, title, children }) => {
+export const LongBigBox = ({ text, title }) => {
   return (
-    <div className="h-full w-full bg-box shadow-xl rounded-2xl p-6">
+    <div className="h-full w-full bg-box shadow-xl rounded-2xl p-6 flex flex-col">
       {/* LongBigBoxTitle */}
       <div className="flex flex-row space-x-4 items-center">
         <Icon name={text} size="4xl" color="white" />
         <p className="font-title text-2xl lg:text-3xl xl:text-4xl font-normal">
           {title}
         </p>
+      </div>
+      <div className="flex flex-col justify-evenly h-full">
+        <Checkbox color="lightBlue" text="RGB" id="rgb" />
+        <Checkbox color="lightBlue" text="NVDI" id="nvdi" />
+        <Checkbox color="lightBlue" text="FLIR" id="flir" />
       </div>
     </div>
   );
