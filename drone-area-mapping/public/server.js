d5e36e954e9cc54ef6211bc7127521c931229c22
Xytrical <ixandermc@gmail.com>
2021-11-16 11:51:03 +0100

    lel

--- drone-area-mapping/public/server.js
+++ drone-area-mapping/public/server.js
@@ -2,9 +2,27 @@ const net = require('net');
 
 (function () {
     const server = net.createServer(function (socket) {
+        socket.on('end', () => console.log('Client disconnected 😔'));
+        socket.on('data', (data) => console.log(`You sent the following: ${data}`));
         socket.write('Echo server\r\n');
-        socket.pipe(socket);
     });
 
     server.listen(1337, '127.0.0.1');
 }())
+
+var server = net.createServer(function (c) { //'connection' listener
+    console.log('client connected');
+    c.on('end', function () {
+        console.log('client disconnected');
+    });
+    c.write('hello\r\n');
+
+    c.on('data', function (data) {
+        var read = data.toString();
+        console.log(read);
+        // var message = c.read();
+        // console.log(message);
+    })
+    // c.pipe(c);
+    c.write('Hello back to C++'); // But only if you shut down the server
+});
