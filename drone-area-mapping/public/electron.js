2ead619335a7699cb509093e74fa8d96e9c3b9bc
Xytrical <ixandermc@gmail.com>
2021-11-16 14:13:43 +0100

    changed stuff

--- drone-area-mapping/public/electron.js
+++ drone-area-mapping/public/electron.js
@@ -1,11 +1,10 @@
 const path = require('path');
-const server = require('./server');
+const net = require('net');
 
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 
 function createWindow() {
-  // Create the browser window.
   const win = new BrowserWindow({
     title: "Drone Area Mapping",
     width: 1280,
@@ -21,20 +20,14 @@ function createWindow() {
       ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
-  // Open the DevTools.
+
   if (isDev) {
     win.webContents.openDevTools({ mode: 'detach' });
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -46,3 +39,11 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+const server = net.createServer(function (socket) {
+  socket.on('end', () => console.log('Client disconnected 😔'));
+  socket.on('data', (data) => console.log(`You sent the following: ${data}`));
+  socket.write('Echo server\r\n');
+});
+
+server.listen(1337, '127.0.0.1');
